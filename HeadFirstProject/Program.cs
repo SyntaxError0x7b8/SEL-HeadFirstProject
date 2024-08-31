b32e9da0b5b263ecb746b1557f30728ffeba9485
jtmen <jtmen@DESKTOP-C0B5UKI>
2024-08-31 07:43:05 +0100

    Add project files.

---
+++ HeadFirstProject/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace HeadFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OperatorExamples();
+        }
+
+        private static void OperatorExamples()
+        {
+            int width = 3;
+            width++;
+
+            int height = 2 + 4;
+            int area = width * height;
+            Console.WriteLine(area);
+
+            string result = "The area";
+            result = result + " is " + area;
+            Console.WriteLine(result);
+
+            bool truthValue = true;
+            Console.WriteLine(truthValue);
+        }
+    }
+}
